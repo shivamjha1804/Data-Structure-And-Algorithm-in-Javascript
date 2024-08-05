24e54e63c94c0707661a9829a4605ed2c91126c7
shivamjha1804 <jhashivam1804@gmail.com>
2024-08-06 02:19:47 +0530

    Insertation sort source code

---
+++ Algorithm/Insertation sort/insertationSort.js
@@ -0,0 +1,17 @@
+function insertationSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > key) {
+        arr[j + 1] = arr[j];
+      } else {
+        break;
+      }
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+console.log(insertationSort([5, 8, 6, 3, 1, 7]));
