9853ae199d76f824b3ad4b66d539aa4c26fb4a12
shivamjha1804 <jhashivam1804@gmail.com>
2024-08-06 01:49:56 +0530

    Bubble sort

---
+++ Algorithm/Bubble sort/BubbleSort.js
@@ -0,0 +1,20 @@
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2, 6, 7, 3, 5, 1]));
